604260fe2ea0018f2030a01bd47e2eec568cec1c
Anton Manolov <mramanolov@gmail.com>
2023-06-06 11:50:32 +0300

    commiting the new pokemon_class file, withouth the other classes

--- pokemon_battler/pokemon_class.py
+++ pokemon_battler/pokemon_class.py
@@ -1,5 +1,6 @@
 from abc import ABC
 
+#main abstract pokemon class from which every other class is made
 class Pokemon(ABC):
     def __init__(self, name, type, hp, level = 0, battles_won = 0, attacks = None) -> None:
         self.name = name
@@ -26,21 +27,3 @@ class Pokemon(ABC):
             print(f'{self.name} won the battle')
             self.battles_won += 1
             self.level += 1
-    
-    
-class ElectricPokemon(Pokemon):
-    def __init__(self, name, hp, level=0, battles_won=0) -> None:
-        super().__init__(name, 'electric', hp, level, battles_won, {'Electrify': 5, 'Fusion Bolt': 12, 'Zap Cannon': 15, 'Wildbolt Storm': 26})
-
-class FirePokemon(Pokemon):
-    def __init__(self, name, hp, level=0, battles_won=0) -> None:
-        super().__init__(name, 'fire', hp, level, battles_won, {'Fiery Dance': 6, 'Fire Lash': 10, 'Eruption': 12, 'Magma Storm': 40})
-
-class WaterPokemon(Pokemon):
-    def __init__(self, name, hp, level=0, battles_won=0) -> None:
-        super().__init__(name, 'water', hp, level, battles_won, {'Aqua Cutter': 6.7, 'Oceanic Operetta': 10, 'Bubble Beam': 11.5, 'Wave Crash': 27} )
-
-class GhostPokemon(Pokemon):
-    def __init__(self, name, hp, level=0, battles_won=0) -> None:
-        super().__init__(name, 'ghost', hp, level, battles_won, {'Hex': 5.5, 'Ominous Wind': 7.8, 'Phantom Force': 14, 'Infernal Parade': 33})
-
