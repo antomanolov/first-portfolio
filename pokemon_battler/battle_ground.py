64c4a3b900c8c22d41de53822b4d0e56b9c0d3eb
Anton Manolov <mramanolov@gmail.com>
2023-06-06 13:28:02 +0300

    added delay print function in battle_ground

--- pokemon_battler/battle_ground.py
+++ pokemon_battler/battle_ground.py
@@ -1,15 +1,21 @@
 from pokemon_main_types import ElectricPokemon, FirePokemon, WaterPokemon, GhostPokemon
+import time
+import sys
 
+# got this func from here https://stackoverflow.com/questions/9246076/how-to-print-one-character-at-a-time-on-one-line
+# + one additional new line print at the end of the loop
 
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    print('\n')
+
+# test pokemon
 pikachu = ElectricPokemon('Pikachu', 120)
 charmander = FirePokemon('Charmander', 150)
 bulbasour = WaterPokemon('Bulbosour', 130)
 ghastly = GhostPokemon('Ghastly', 90)
 
 charizard = FirePokemon('Charizard', 1500)
-
-print(pikachu.attacks)
-print(charmander.attacks)
-print(bulbasour.attacks)
-print(ghastly.attacks)
-print(charizard.hp)
