ae080206888c13266aa2cb87f267616e0262f16e
Anton Manolov <mramanolov@gmail.com>
2023-06-27 14:39:59 +0300

    making separate functions for every functionality of the code

--- etch-a-sketch/js/script.js
+++ etch-a-sketch/js/script.js
@@ -3,40 +3,54 @@ let btn = document.querySelector('.number-choose')
 let gridNumber = document.querySelector('input')
 let grid = document.querySelector('.square-container')
 
-function putClassSquare () {
+function putClassSquare() {
     const newDiv = document.createElement('div')
     newDiv.classList.add('square')
     return newDiv
 }
 
-
-
-btn.addEventListener('click', () => {
-    grid.innerHTML = ''
-    if(gridNumber.value === '' || gridNumber.value > 100){
+function gridValue() {
+    if (gridNumber.value === '' || gridNumber.value > 100) {
         gridNumber.value = 100;
-    }else if(gridNumber.value < 9){
+    } else if (gridNumber.value < 9) {
         gridNumber.value = 9;
     }
-    grid.style.setProperty("grid-template-columns", `repeat(${gridNumber.value}, 2fr)`);
-    grid.style.setProperty("grid-template-rows", `repeat(${gridNumber.value}, 2fr)`);
+    const value = +gridNumber.value
     
-    for (let i = 0; i < gridNumber.value * gridNumber.value; i++){
-            grid.append(putClassSquare())
+    return value
+}
+
+function gridCreate(numInput, color) {
+    // adding correct properties to the grid
+    grid.style.setProperty("grid-template-columns", `repeat(${numInput}, 2fr)`);
+    grid.style.setProperty("grid-template-rows", `repeat(${numInput}, 2fr)`);
+    for (let i = 0; i < numInput * numInput; i++) {
+        // putting the class square on every div
+        grid.append(putClassSquare())
     }
     allGridSquares = document.querySelectorAll('.square')
     allGridSquares.forEach(square => square.addEventListener(
         'mouseover', () => {
-            if (square.style.backgroundColor === 'black'){
-            square.style.backgroundColor = 'white'
-            }else{
-                square.style.backgroundColor = 'black'
-            };
-        } 
+            square.style.backgroundColor = color
+        }
     ));
-    
-    console.log(allGridSquares)
-    
+}
+
+
+btn.addEventListener('click', () => {
+    // on every button click we delete all divs and make space for new ones
+    grid.innerHTML = ''
+
+    // evaluate grid value properly
+    const userInput = gridValue()
+    gridCreate(userInput, 'black')
+
+
 })
 
-
+let circleColor = document.querySelectorAll('.color')
+circleColor.forEach(cirle => {
+    cirle.addEventListener('click', ()=>{
+        
+    })
+})
