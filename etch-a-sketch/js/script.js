e4c0837fd0f930d4bb36da60c7b41d76c810022e
Anton Manolov <mramanolov@gmail.com>
2023-06-27 15:02:29 +0300

    add refresh button and diff color functionality

--- etch-a-sketch/js/script.js
+++ etch-a-sketch/js/script.js
@@ -1,26 +1,10 @@
 let allGridSquares = document.querySelectorAll('.square')
-let btn = document.querySelector('.number-choose')
+let btn = document.querySelector('.refresh')
 let gridNumber = document.querySelector('input')
 let grid = document.querySelector('.square-container')
+let circleColor = document.querySelectorAll('.color')
 
-function putClassSquare() {
-    const newDiv = document.createElement('div')
-    newDiv.classList.add('square')
-    return newDiv
-}
-
-function gridValue() {
-    if (gridNumber.value === '' || gridNumber.value > 100) {
-        gridNumber.value = 100;
-    } else if (gridNumber.value < 9) {
-        gridNumber.value = 9;
-    }
-    const value = +gridNumber.value
-    
-    return value
-}
-
-function gridCreate(numInput, color) {
+function gridCreate(numInput) {
     // adding correct properties to the grid
     grid.style.setProperty("grid-template-columns", `repeat(${numInput}, 2fr)`);
     grid.style.setProperty("grid-template-rows", `repeat(${numInput}, 2fr)`);
@@ -28,16 +12,43 @@ function gridCreate(numInput, color) {
         // putting the class square on every div
         grid.append(putClassSquare())
     }
+    
+};
+
+
+function putClassSquare() {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('square');
+    return newDiv;
+};
+
+function circleColoring (color) {
     allGridSquares = document.querySelectorAll('.square')
     allGridSquares.forEach(square => square.addEventListener(
         'mouseover', () => {
             square.style.backgroundColor = color
         }
     ));
-}
+};
+
+// getting the right min and max grid value without fail
+function gridValue() {
+    if (gridNumber.value === '' || gridNumber.value > 100) {
+        gridNumber.value = 100;
+    } else if (gridNumber.value < 9) {
+        gridNumber.value = 9;
+    };
+    const value = +gridNumber.value;
+    
+    return value;
+};
 
 
-btn.addEventListener('click', () => {
+
+// trying the 'change' event, maybe click is better cuz when refreshed you 
+// can't output the same grid again before putting new value
+
+gridNumber.addEventListener('change', () => {
     // on every button click we delete all divs and make space for new ones
     grid.innerHTML = ''
 
@@ -48,9 +59,14 @@ btn.addEventListener('click', () => {
 
 })
 
-let circleColor = document.querySelectorAll('.color')
+// adding the right color value to mouseover
 circleColor.forEach(cirle => {
     cirle.addEventListener('click', ()=>{
-        
+        circleColoring(cirle.classList[0]);
     })
+})
+
+// refresh button
+btn.addEventListener('click', () =>{
+    grid.innerHTML = ''
 })
