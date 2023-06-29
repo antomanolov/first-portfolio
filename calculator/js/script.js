572825f26caf94ed8257a466285c8b236fbe658a
Anton Manolov <mramanolov@gmail.com>
2023-06-29 11:56:01 +0300

    trying to make the complicated summing works

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -1,13 +1,36 @@
+function evaluate (arr) {
+    
+    let equality = arr.split(' ')
+    if(equality[1] == '+'){
+        sum = parseInt(equality[0]) + parseInt(equality[2])
+    }else if(equality[1] == '-'){
+        sum = parseInt(equality[0]) - parseInt(equality[2])
+    }else if(equality[1] == '*'){
+        sum = parseInt(equality[0]) * parseInt(equality[2])
+    }else{
+        sum = parseInt(equality[0]) / parseInt(equality[2])
+    }
+    return sum
+}
+
 const allNumbers = document.querySelectorAll('.number')
 const display = document.querySelector('.display p')
 const clearBtn = document.querySelector('.clear')
 
-const sumButton = document.querySelector
+const division = document.querySelector('.division')
+const multiply = document.querySelector('.multiply')
+const add = document.querySelector('.add')
+const subtract = document.querySelector('.subtract')
+const equal = document.querySelector('.equal')
+
+
+// const sumButton = document.querySelector
 
 let sum = 0
 
 clearBtn.addEventListener('click', () => {
     display.innerHTML = '0'
+    sum = 0
 })
 
 allNumbers.forEach(number => number.addEventListener('click', () =>{
@@ -18,4 +41,42 @@ allNumbers.forEach(number => number.addEventListener('click', () =>{
         display.textContent += number.value
     }
 }))
+
+add.addEventListener('click', () =>{
+    sum += display.textContent
+    sum +=' + '
+    console.log(sum)
+   
+    display.textContent = evaluate(sum)
+    
+    display.innerHTML = ''
+    
+})
+
+
+subtract.addEventListener('click', () =>{
+    sum = display.textContent
+    sum +=' - '
+    display.innerHTML = ''
+    evaluate(sum)
+})
+
+multiply.addEventListener('click', () =>{
+    sum = display.textContent
+    sum +=' * '
+    display.innerHTML = ''
+    evaluate(sum)
+})
+
+division.addEventListener('click', () =>{
+    sum = display.textContent
+    sum +=' / '
+    display.innerHTML = ''
+    evaluate(sum)
+})
+
+equal.addEventListener('click', () =>{
+    sum += display.textContent
+    display.textContent = evaluate(sum)
+})
 
