e8bce04fb1f953181b7bf23dfafd657adf39311c
Anton Manolov <mramanolov@gmail.com>
2023-06-29 14:20:09 +0300

    working with bugs ver

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -1,15 +1,38 @@
-function evaluate (arr) {
+// TODO make numbers dissapear after clicking the sign 
+// OR after clicking sing and starting to write
+// make (%, +/- and .) work
+function evaluate(arr) {
     
-    let equality = arr.split(' ')
-    if(equality[1] == '+'){
-        sum = parseInt(equality[0]) + parseInt(equality[2])
-    }else if(equality[1] == '-'){
-        sum = parseInt(equality[0]) - parseInt(equality[2])
-    }else if(equality[1] == '*'){
-        sum = parseInt(equality[0]) * parseInt(equality[2])
-    }else{
-        sum = parseInt(equality[0]) / parseInt(equality[2])
+    while (arr[arr.length - 1] == '*' ||
+        arr[arr.length - 1] == '-' ||
+        arr[arr.length - 1] == '+' ||
+        arr[arr.length - 1] == '/' ||
+        arr[arr.length - 1] == '') {
+        arr.pop()
     }
+    console.log(arr)
+    for (let i = 0; i < arr.length - 2; i += 2) {
+        let currSum = 0
+        let num1 = parseInt(arr[i])
+        let num2 = parseInt(arr[i+ 2])
+        if (arr[i + 1] == '+') {
+            currSum = num1 + num2
+            arr[i + 2] = currSum
+
+        } else if (arr[i + 1] == '*') {
+            currSum = arr[i] * arr[i + 2]
+            arr[i + 2] = currSum
+        } else if (arr[i + 1] == '-') {
+            currSum = arr[i] - arr[i + 2]
+            arr[i + 2] = currSum
+        } else if (arr[i + 1] == '/') {
+            currSum = arr[i] / arr[i + 2]
+            arr[i + 2] = currSum
+        } 
+        sum = currSum
+    }
+    string = ''
+    
     return sum
 }
 
@@ -25,58 +48,72 @@ const equal = document.querySelector('.equal')
 
 
 // const sumButton = document.querySelector
-
+let firstLen = 1
 let sum = 0
+let string = ''
 
 clearBtn.addEventListener('click', () => {
     display.innerHTML = '0'
     sum = 0
 })
 
-allNumbers.forEach(number => number.addEventListener('click', () =>{
+allNumbers.forEach(number => number.addEventListener('click', () => {
     if(display.textContent == 0){
         display.innerHTML = ''
     }
-    if(display.textContent.length <= 11) {
+    if (display.textContent.length <= 11) {
         display.textContent += number.value
     }
+    
 }))
 
-add.addEventListener('click', () =>{
-    sum += display.textContent
-    sum +=' + '
-    console.log(sum)
-   
-    display.textContent = evaluate(sum)
+add.addEventListener('click', () => {
+    string += display.textContent
+    string += ' + '
     
     display.innerHTML = ''
+    if(string.split(' ').length > 4) {
+        display.textContent = `${evaluate(string.split(' '))}`
+        
+    }
+    
     
 })
 
 
-subtract.addEventListener('click', () =>{
-    sum = display.textContent
-    sum +=' - '
+subtract.addEventListener('click', () => {
+    string += display.textContent
+    string += ' - '
     display.innerHTML = ''
-    evaluate(sum)
+    if(string.split(' ').length > 3) {
+        display.textContent = `${evaluate(string.split(' '))}`
+    }
 })
 
-multiply.addEventListener('click', () =>{
-    sum = display.textContent
-    sum +=' * '
+multiply.addEventListener('click', () => {
+    string += display.textContent
+    string += ' * '
     display.innerHTML = ''
-    evaluate(sum)
+    if(string.split(' ').length > 3) {
+        display.textContent = `${evaluate(string.split(' '))}`
+    }
 })
 
-division.addEventListener('click', () =>{
-    sum = display.textContent
-    sum +=' / '
+division.addEventListener('click', () => {
+    string += display.textContent
+    string += ' / '
     display.innerHTML = ''
-    evaluate(sum)
+    if(string.split(' ').length > 3) {
+        display.textContent = `${evaluate(string.split(' '))}`
+    }
 })
 
-equal.addEventListener('click', () =>{
-    sum += display.textContent
-    display.textContent = evaluate(sum)
+equal.addEventListener('click', () => {
+    string += display.textContent
+    console.log(string)
+    if(string.split(' ').length === 3) {
+        display.textContent = `${evaluate(string.split(' '))}`
+    }
+    string = ''
 })
 
